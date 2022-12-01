fff95ab16b41bac8be1ff8609e4bb725db560f19
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-12-01 15:25:34 +0100

    Reorder things and make the teaser schema work

--- src/components/Blocks/Teaser/schema.js
+++ src/components/Blocks/Teaser/schema.js
@@ -1,19 +1,50 @@
-export const TeaserStylingSchema = ({ schema, formData, intl }) => {
-  schema.properties.align.widget = 'inner_align';
-  schema.properties.align.default = 'left';
-  schema.properties.align.actions = ['left', 'right', 'center'];
+import { defineMessages } from 'react-intl';
+import { defaultStylingSchema } from '../schema';
+
+const messages = defineMessages({
+  modifiedHeadTitle: {
+    id: 'Date / Time',
+    defaultMessage: 'Date / Time',
+  },
+  align: {
+    id: 'Alignment',
+    defaultMessage: 'Alignment',
+  },
+});
+
+export const teaserSchemaEnhancer = ({ schema, formData, intl }) => {
+  schema.properties.href.selectedItemAttrs.push('getRemoteUrl');
+  if (formData.href?.[0]?.['@type'] === 'News Item') {
+    // remove head_title
+    schema.fieldsets[0].fields = [
+      'href',
+      'title',
+      'description',
+      'preview_image',
+    ];
+  }
+  schema.properties.head_title.title = intl.formatMessage(
+    messages.modifiedHeadTitle,
+  );
+
+  defaultStylingSchema({ schema, formData, intl });
+
+  schema.properties.styles.schema.fieldsets[0].fields = [
+    'align',
+    'backgroundColor',
+  ];
+
+  schema.properties.styles.schema.properties.align = {
+    widget: 'align',
+    title: intl.formatMessage(messages.align),
+    actions: ['left', 'right', 'center'],
+    default: 'left',
+  };
+
   return schema;
 };
 
-// export const teaserSchemaEnhancer = ({ schema, formData, intl }) => {
-//   if (formData.href?.[0]?.['@type'] === 'News Item') {
-//     // remove head_title
-//     schema.fieldsets[0].fields = [
-//       'href',
-//       'title',
-//       'description',
-//       'preview_image',
-//     ];
-//   }
-//   return schema;
-// };
+export const gridTeaserDisableStylingSchema = ({ schema, formData, intl }) => {
+  schema.fieldsets = schema.fieldsets.filter((item) => item.id !== 'styling');
+  return schema;
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import { TeaserStylingSchema } from './components/Blocks/Teaser/schema';
-import AlignWidget from './components/Widgets/AlignWidget';
+import { defaultStylingSchema } from './components/Blocks/schema';
+import {
+  gridTeaserDisableStylingSchema,
+  teaserSchemaEnhancer,
+} from './components/Blocks/Teaser/schema';
+import gridSVG from './icons/block_icn_grid.svg';
+
+const BG_COLORS = [
+  { name: 'transparent', label: 'Transparent' },
+  { name: 'grey', label: 'Grey' },
+];
 
 const applyConfig = (config) => {
   config.settings = {
@@ -7,27 +16,28 @@ const applyConfig = (config) => {
     slidingSearchAnimation: true,
   };
 
-  config.widgets.widget.inner_align = AlignWidget;
-
   config.blocks.blocksConfig.__grid = {
     ...config.blocks.blocksConfig.__grid,
+    colors: BG_COLORS,
+    schemaEnhancer: defaultStylingSchema,
+    icon: gridSVG,
+    gridAllowedBlocks: ['teaser', 'image', 'slate'],
     // One could customize the blocks inside the grid like this:
     blocksConfig: {
       ...config.blocks.blocksConfig,
       teaser: {
         ...config.blocks.blocksConfig.teaser,
-        enableStyling: false,
+        schemaEnhancer: gridTeaserDisableStylingSchema,
       },
     },
   };
 
   config.blocks.blocksConfig.teaser = {
     ...config.blocks.blocksConfig.teaser,
-    enableStyling: true,
-    stylesSchema: TeaserStylingSchema,
     group: 'teasers',
     imageScale: 'larger',
-    // schemaEnhancer: teaserSchemaEnhancer,
+    colors: BG_COLORS,
+    schemaEnhancer: teaserSchemaEnhancer,
   };
 
   return config;
