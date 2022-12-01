2f17d2909e25dbaa1dba1dd583f8395840cec03a
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-12-01 15:01:22 +0100

    Cypress config

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
