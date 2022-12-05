a501da90e4cef0510f494c3924046d0b294a4369
Érico Andrei <ericof@gmail.com>
2022-12-05 12:43:10 -0300

    Remove .eslintrc.js, as it breaks other projects

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
-    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/reset-fixture.js
+++ cypress/support/reset-fixture.js
@@ -1,30 +1,32 @@
 function setup() {
-  const api_url = Cypress.env("API_PATH") || "http://localhost:55001/plone";
+  const api_url = Cypress.env('API_PATH') || 'http://localhost:55001/plone';
   cy.request({
-    method: "POST",
+    method: 'POST',
     url: `${api_url}/RobotRemote`,
-    headers: { Accept: "text/xml", "content-type": "text/xml" },
-    body: '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_setup</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
-  }).then(() => cy.log("Setting up API fixture"));
+    headers: { Accept: 'text/xml', 'content-type': 'text/xml' },
+    body:
+      '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_setup</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
+  }).then(() => cy.log('Setting up API fixture'));
 }
 
 function teardown() {
-  const api_url = Cypress.env("API_PATH") || "http://localhost:55001/plone";
+  const api_url = Cypress.env('API_PATH') || 'http://localhost:55001/plone';
   cy.request({
-    method: "POST",
+    method: 'POST',
     url: `${api_url}/RobotRemote`,
-    headers: { Accept: "text/xml", "content-type": "text/xml" },
-    body: '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_teardown</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
-  }).then(() => cy.log("Tearing down API fixture"));
+    headers: { Accept: 'text/xml', 'content-type': 'text/xml' },
+    body:
+      '<?xml version="1.0"?><methodCall><methodName>run_keyword</methodName><params><param><value><string>remote_zodb_teardown</string></value></param><param><value><array><data><value><string>plone.app.robotframework.testing.PLONE_ROBOT_TESTING</string></value></data></array></value></param></params></methodCall>',
+  }).then(() => cy.log('Tearing down API fixture'));
 }
 
 function main() {
   const command = process.argv[2];
   switch (command) {
-    case "setup":
+    case 'setup':
       setup();
       break;
-    case "teardown":
+    case 'teardown':
       teardown();
       break;
     default:
