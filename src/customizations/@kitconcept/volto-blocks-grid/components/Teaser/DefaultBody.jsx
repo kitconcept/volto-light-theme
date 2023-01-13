92a6da4b3c819fbe7a78b820445591b58747535f
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-01-13 15:37:13 +0100

    Add temporary fix for images in teasers

---
+++ src/customizations/@kitconcept/volto-blocks-grid/components/Teaser/DefaultBody.jsx
@@ -0,0 +1,85 @@
+// Remove when https://github.com/kitconcept/volto-light-theme/issues/40
+// is solved and merged
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
+import { defineMessages, useIntl } from 'react-intl';
+import imageBlockSVG from '@plone/volto/components/manage/Blocks/Image/block-image.svg';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { getTeaserImageURL } from '@kitconcept/volto-blocks-grid/components/Teaser/utils';
+import { MaybeWrap } from '@plone/volto/components';
+import { UniversalLink } from '@plone/volto/components';
+import cx from 'classnames';
+import config from '@plone/volto/registry';
+
+const messages = defineMessages({
+  PleaseChooseContent: {
+    id: 'Please choose an existing content as source for this element',
+    defaultMessage:
+      'Please choose an existing content as source for this element',
+  },
+});
+
+const DefaultImage = (props) => <img {...props} alt={props.alt || ''} />;
+
+const TeaserDefaultTemplate = (props) => {
+  const { className, data, isEditMode } = props;
+  const intl = useIntl();
+  const href = data.href?.[0];
+  const image = data.preview_image?.[0];
+  const align = data?.styles?.align;
+
+  const Image = DefaultImage;
+
+  return (
+    <div className={cx('block teaser', className)}>
+      <>
+        {!href && isEditMode && (
+          <Message>
+            <div className="grid-teaser-item placeholder">
+              <img src={imageBlockSVG} alt="" />
+              <p>{intl.formatMessage(messages.PleaseChooseContent)}</p>
+            </div>
+          </Message>
+        )}
+        {href && (
+          <MaybeWrap
+            condition={!isEditMode}
+            as={UniversalLink}
+            href={href['@id']}
+            target={data.openLinkInNewTab ? '_blank' : null}
+          >
+            <div className="grid-teaser-item default">
+              {(href.hasPreviewImage || href.image_field || image) && (
+                <div className="grid-image-wrapper">
+                  <Image
+                    src={flattenToAppURL(
+                      getTeaserImageURL({ href, image, align }),
+                    )}
+                    alt=""
+                    loading="lazy"
+                  />
+                </div>
+              )}
+              <div className="content">
+                {data?.head_title && (
+                  <div className="headline">{data.head_title}</div>
+                )}
+
+                <h2>{data?.title}</h2>
+                {!data.hide_description && <p>{data?.description}</p>}
+              </div>
+            </div>
+          </MaybeWrap>
+        )}
+      </>
+    </div>
+  );
+};
+
+TeaserDefaultTemplate.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+  isEditMode: PropTypes.bool,
+};
+
+export default TeaserDefaultTemplate;
