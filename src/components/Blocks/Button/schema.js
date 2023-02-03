075ac82e46464225855fce7c196e4afe047e11d7
iRohitSingh <61353484+iRohitSingh@users.noreply.github.com>
2023-02-04 03:25:53 +0530

    Add style to Button block (#63) (#64)
    Co-authored-by: Dante Álvarez <89805481+danalvrz@users.noreply.github.com>

---
+++ src/components/Blocks/Button/schema.js
@@ -0,0 +1,23 @@
+import { defineMessages } from 'react-intl';
+import { defaultStylingSchema } from '../schema';
+
+const messages = defineMessages({
+  Align: {
+    id: 'Alignment',
+    defaultMessage: 'Alignment',
+  },
+});
+
+export const ButtonStylingSchema = ({ schema, formData, intl }) => {
+  defaultStylingSchema({ schema, formData, intl });
+
+  schema.properties.styles.schema.fieldsets[0].fields = ['buttonAlign'];
+
+  schema.properties.styles.schema.properties.buttonAlign = {
+    widget: 'align',
+    title: intl.formatMessage(messages.Align),
+    actions: ['center', 'wide'],
+  };
+
+  return schema;
+};
