00a9694f814a58678c474e99a6427db13791808e
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-05-05 16:15:01 -0600

    fix ListingBody to show headline

--- src/customizations/volto/components/manage/Blocks/Listing/ListingBody.jsx
+++ src/customizations/volto/components/manage/Blocks/Listing/ListingBody.jsx
@@ -38,89 +38,97 @@ const ListingBody = withQuerystringResults((props) => {
   }
 
   const listingRef = createRef();
-  return listingItems?.length > 0 ? (
-    <div ref={listingRef}>
-      <ListingBodyTemplate
-        items={listingItems}
-        isEditMode={isEditMode}
-        {...data}
-      />
-      {totalPages > 1 && (
-        <div className="pagination-wrapper">
-          <Pagination
-            activePage={currentPage}
-            totalPages={totalPages}
-            onPageChange={(e, { activePage }) => {
-              !isEditMode &&
-                listingRef.current.scrollIntoView({ behavior: 'smooth' });
-              onPaginationChange(e, { activePage });
-            }}
-            firstItem={null}
-            lastItem={null}
-            prevItem={{
-              content: (
-                <FormattedMessage
-                  id="Previous Page"
-                  defaultMessage="Previous Page"
-                />
-              ),
-              icon: true,
-              'aria-disabled': !prevBatch,
-              className: !prevBatch ? 'disabled' : null,
-            }}
-            nextItem={{
-              content: (
-                <FormattedMessage id="Next Page" defaultMessage="Next Page" />
-              ),
-              icon: true,
-              'aria-disabled': !nextBatch,
-              className: !nextBatch ? 'disabled' : null,
-            }}
+  return (
+    <>
+      {data.headline && <h2 className="headline">{data.headline}</h2>}
+      {listingItems?.length > 0 ? (
+        <div ref={listingRef}>
+          <ListingBodyTemplate
+            items={listingItems}
+            isEditMode={isEditMode}
+            {...data}
           />
-          <div className="total">
-            <FormattedMessage id="Result" defaultMessage="Result" />{' '}
-            {(currentPage - 1) * batch_size + 1}-
-            {(currentPage - 1) * batch_size + listingItems.length}{' '}
-            <FormattedMessage id="of" defaultMessage="of" />{' '}
-            {total || properties.items_total}
-          </div>
+          {totalPages > 1 && (
+            <div className="pagination-wrapper">
+              <Pagination
+                activePage={currentPage}
+                totalPages={totalPages}
+                onPageChange={(e, { activePage }) => {
+                  !isEditMode &&
+                    listingRef.current.scrollIntoView({ behavior: 'smooth' });
+                  onPaginationChange(e, { activePage });
+                }}
+                firstItem={null}
+                lastItem={null}
+                prevItem={{
+                  content: (
+                    <FormattedMessage
+                      id="Previous Page"
+                      defaultMessage="Previous Page"
+                    />
+                  ),
+                  icon: true,
+                  'aria-disabled': !prevBatch,
+                  className: !prevBatch ? 'disabled' : null,
+                }}
+                nextItem={{
+                  content: (
+                    <FormattedMessage
+                      id="Next Page"
+                      defaultMessage="Next Page"
+                    />
+                  ),
+                  icon: true,
+                  'aria-disabled': !nextBatch,
+                  className: !nextBatch ? 'disabled' : null,
+                }}
+              />
+              <div className="total">
+                <FormattedMessage id="Result" defaultMessage="Result" />{' '}
+                {(currentPage - 1) * batch_size + 1}-
+                {(currentPage - 1) * batch_size + listingItems.length}{' '}
+                <FormattedMessage id="of" defaultMessage="of" />{' '}
+                {total || properties.items_total}
+              </div>
+            </div>
+          )}
+        </div>
+      ) : isEditMode ? (
+        <div className="listing message" ref={listingRef}>
+          {isFolderContentsListing && (
+            <FormattedMessage
+              id="No items found in this container."
+              defaultMessage="No items found in this container."
+            />
+          )}
+          {hasLoaded && (
+            <FormattedMessage
+              id="No results found."
+              defaultMessage="No results found."
+            />
+          )}
+          <Dimmer active={!hasLoaded} inverted>
+            <Loader indeterminate size="small">
+              <FormattedMessage id="loading" defaultMessage="Loading" />
+            </Loader>
+          </Dimmer>
+        </div>
+      ) : (
+        <div className="listing message" ref={listingRef}>
+          {hasLoaded && (
+            <FormattedMessage
+              id="No results found."
+              defaultMessage="No results found."
+            />
+          )}
+          <Dimmer active={!hasLoaded} inverted>
+            <Loader indeterminate size="small">
+              <FormattedMessage id="loading" defaultMessage="Loading" />
+            </Loader>
+          </Dimmer>
         </div>
       )}
-    </div>
-  ) : isEditMode ? (
-    <div className="listing message" ref={listingRef}>
-      {isFolderContentsListing && (
-        <FormattedMessage
-          id="No items found in this container."
-          defaultMessage="No items found in this container."
-        />
-      )}
-      {hasLoaded && (
-        <FormattedMessage
-          id="No results found."
-          defaultMessage="No results found."
-        />
-      )}
-      <Dimmer active={!hasLoaded} inverted>
-        <Loader indeterminate size="small">
-          <FormattedMessage id="loading" defaultMessage="Loading" />
-        </Loader>
-      </Dimmer>
-    </div>
-  ) : (
-    <div className="listing message" ref={listingRef}>
-      {hasLoaded && (
-        <FormattedMessage
-          id="No results found."
-          defaultMessage="No results found."
-        />
-      )}
-      <Dimmer active={!hasLoaded} inverted>
-        <Loader indeterminate size="small">
-          <FormattedMessage id="loading" defaultMessage="Loading" />
-        </Loader>
-      </Dimmer>
-    </div>
+    </>
   );
 });
 
