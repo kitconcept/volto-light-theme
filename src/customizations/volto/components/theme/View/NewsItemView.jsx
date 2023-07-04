ac127d071137e726985d00d843015ec218da7de4
iFlameing <ialokkumarsingh0@gmail.com>
2023-07-04 16:25:11 +0530

    News Item view

---
+++ src/customizations/volto/components/theme/View/NewsItemView.jsx
@@ -0,0 +1,60 @@
+/**
+ * OVERRIDE NewsItemView.jsx
+ * REASON: BFS theme
+ * DATE: 2023-07-04
+ * DEVELOPER: @IFlameing
+ */
+
+/**
+ * NewsItemView view component.
+ * @module components/theme/View/NewsItemView
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
+import { format, parse } from 'date-fns';
+
+/**
+ * NewsItemView view component class.
+ * @function NewsItemView
+ * @params {object} content Content object.
+ * @returns {string} Markup of the component.
+ */
+const NewsItemView = ({ content }) => {
+  var deLocale = require('date-fns/locale/de');
+  return (
+    <div id="page-document" className="ui container viewwrapper event-view">
+      <div className="dates">
+        {content?.effective ? (
+          <span className="day">
+            {format(parse(content?.effective), 'D.MM.YYYY', {
+              locale: deLocale,
+            })}{' '}
+          </span>
+        ) : (
+          <span className="day">No date</span>
+        )}{' '}
+        <span className="headtitle">| {content?.head_title}</span>
+      </div>
+      <RenderBlocks content={content} />
+    </div>
+  );
+};
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+NewsItemView.propTypes = {
+  content: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    text: PropTypes.shape({
+      data: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default NewsItemView;
