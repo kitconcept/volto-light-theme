08e89941689fe8454995467d15bb4d5774c05c89
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-07-06 19:24:12 -0600

    style fixes

---
+++ src/components/Blocks/Listing/View.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { withBlockExtensions } from '@plone/volto/helpers';
+
+import { ListingBlockBody as ListingBody } from '@plone/volto/components';
+
+const View = (props) => {
+  const { data, path, pathname, className } = props;
+
+  return (
+    <div
+      className={cx(
+        'block listing',
+        data.variation || 'default',
+        data.headline ? '' : 'no-headline',
+        className,
+      )}
+    >
+      <ListingBody {...props} path={path ?? pathname} />
+    </div>
+  );
+};
+
+View.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+  properties: PropTypes.objectOf(PropTypes.any).isRequired,
+  block: PropTypes.string,
+};
+
+export default withBlockExtensions(View);
