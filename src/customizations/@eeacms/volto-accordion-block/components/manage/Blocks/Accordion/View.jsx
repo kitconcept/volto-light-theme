0a67583d3b9cf2912e406c8ad5cb20987b53f70d
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-07-14 14:21:24 -0600

    remove customization

--- src/customizations/@eeacms/volto-accordion-block/components/manage/Blocks/Accordion/View.jsx
+++
@@ -1,8 +0,0 @@
-/**
- * OVERRIDE View.jsx
- * REASON: Change to use className prop from StyleWrapper.
- */
-
-import View from '../../../../../../../components/Blocks/Accordion/View';
-
-export default View;
