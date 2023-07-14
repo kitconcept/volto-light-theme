7c4c97a25a0a86de919965b98fa1bfbee2bcb5da
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-07-13 18:25:25 -0600

    include classNames from StyleWrapper

---
+++ src/customizations/@eeacms/volto-accordion-block/components/manage/Blocks/Accordion/View.jsx
@@ -0,0 +1,8 @@
+/**
+ * OVERRIDE View.jsx
+ * REASON: Change to use className prop from StyleWrapper.
+ */
+
+import View from '../../../../../../../components/Blocks/Accordion/View';
+
+export default View;
