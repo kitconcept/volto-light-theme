6b72950215469688122f3185ef1bb363692039fd
David Glick <david@glicksoftware.com>
2023-08-09 11:42:22 -0700

    fix translations

--- src/components/Blocks/Toc/schema.js
+++ src/components/Blocks/Toc/schema.js
@@ -1,3 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  toc: {
+    id: 'toc',
+    defaultMessage: 'Table of Contents',
+  },
+  Title: {
+    id: 'Title',
+    defaultMessage: 'Title',
+  },
+});
+
 export const tocBlockSchemaEnhancer = ({ formData, schema, intl }) => {
   schema.fieldsets[0].fields = ['title'];
   schema.properties.levels.choices = [
@@ -5,5 +18,10 @@ export const tocBlockSchemaEnhancer = ({ formData, schema, intl }) => {
     ['h3', 'h3'],
   ];
   schema.properties.levels.default = ['h2', 'h3'];
+
+  // can be removed after https://github.com/plone/volto/pull/5073 is released
+  schema.title = intl.formatMessage(messages.toc);
+  schema.properties.title.title = intl.formatMessage(messages.Title);
+
   return schema;
 };
