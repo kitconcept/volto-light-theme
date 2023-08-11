6cfb4ca18fa4816c0f5f8b0325ca293dc89177f1
iRohitSingh <isinghrohit0@gmail.com>
2023-08-11 11:34:57 +0530

    Fix Translations for the accordeon block(#53)

--- src/components/Blocks/Accordion/schema.jsx
+++ src/components/Blocks/Accordion/schema.jsx
@@ -1,3 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  addAccordion: {
+    id: 'Add Accordion',
+    defaultMessage: 'Add Accordion',
+  },
+});
+
 export function AccordionSchemaEnhancer({ schema, formData, intl }) {
   // Opinionated removal of the options fieldset
   schema.fieldsets = schema.fieldsets.filter((item) => item.id !== 'options');
@@ -6,6 +15,9 @@ export function AccordionSchemaEnhancer({ schema, formData, intl }) {
   schema.properties.right_arrows.default = true;
   schema.properties.collapsed.default = false;
   schema.properties.non_exclusive.default = false;
+  schema.properties.data.schema.addMessage = intl.formatMessage(
+    messages.addAccordion,
+  );
 
   return schema;
 }
