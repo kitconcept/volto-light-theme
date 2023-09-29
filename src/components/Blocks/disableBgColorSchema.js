73a8f0cb941a4e6773611558e03c405434a6ed17
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-09-29 20:20:50 +0200

    Update to use the proper linter versions and correct violations (#245)

--- src/components/Blocks/disableBgColorSchema.js
+++ src/components/Blocks/disableBgColorSchema.js
@@ -1,13 +1,12 @@
 export const disableBgColorSchema = ({ schema }) => {
-  const {
-    backgroundColor,
-    ...filteredSchemaProperties
-  } = schema.properties.styles.schema.properties;
+  const { backgroundColor, ...filteredSchemaProperties } =
+    schema.properties.styles.schema.properties;
   schema.properties.styles.schema.properties = filteredSchemaProperties;
 
-  schema.properties.styles.schema.fieldsets[0].fields = schema.properties.styles.schema.fieldsets[0].fields.filter(
-    (item) => item !== 'backgroundColor',
-  );
+  schema.properties.styles.schema.fieldsets[0].fields =
+    schema.properties.styles.schema.fieldsets[0].fields.filter(
+      (item) => item !== 'backgroundColor',
+    );
 
   return schema;
 };
