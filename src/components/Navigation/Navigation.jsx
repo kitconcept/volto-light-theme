16ac1c6b7942405e0354edbc5b8ef2ebd3c20ff9
David Ichim <ichim.david@gmail.com>
2023-11-24 17:25:25 +0200

    Navigation close icon is now a button that can be focused and closed by keyboard with translated aria-label

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -21,6 +21,10 @@ const messages = defineMessages({
     id: 'Overview',
     defaultMessage: 'Overview',
   },
+  closeMenu: {
+    id: 'Close menu',
+    defaultMessage: 'Close menu',
+  },
 });
 
 const Navigation = ({ getNavigation, pathname, items, lang }) => {
@@ -100,13 +104,6 @@ const Navigation = ({ getNavigation, pathname, items, lang }) => {
                         active: desktopMenuOpen === index,
                       })}
                     >
-                      <div
-                        role="presentation"
-                        className="close"
-                        onClick={closeMenu}
-                      >
-                        <Icon name={clearSVG} size="48px" />
-                      </div>
                       <div className="submenu-inner">
                         <NavLink
                           to={item.url === '' ? '/' : item.url}
@@ -118,6 +115,13 @@ const Navigation = ({ getNavigation, pathname, items, lang }) => {
                             {intl.formatMessage(messages.overview)})
                           </h2>
                         </NavLink>
+                        <button
+                          className="close"
+                          onClick={closeMenu}
+                          aria-label={intl.formatMessage(messages.closeMenu)}
+                        >
+                          <Icon name={clearSVG} size="48px" />
+                        </button>
                         <ul>
                           {item.items &&
                             item.items.length > 0 &&
