b37dd01ba9e1d5df1fe922db0c7cfe2487210889
Molochem <lxndrnmnn@web.de>
2023-12-28 15:40:11 +0100

    typo fix

--- src/customizations/volto/components/theme/LanguageSelector/LanguageSelector.jsx
+++ src/customizations/volto/components/theme/LanguageSelector/LanguageSelector.jsx
@@ -1,5 +1,5 @@
 /**
- * OVERRIDE Header.jsx
+ * OVERRIDE LanguageSelector.js
  * REASON: This theme uses a custom pre-@plone/components component
  * SemanticUI-free located at the components folder.
  * To override it, override the @kitconcept/volto-light-theme one instead of
