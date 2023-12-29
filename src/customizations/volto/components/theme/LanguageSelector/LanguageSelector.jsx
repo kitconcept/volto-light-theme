6d2662e3d597aaee97c9b10a2d602a7a717704ec
Alexander Neumann <140883099+Molochem@users.noreply.github.com>
2023-12-29 10:49:10 +0100

    Updated the Language Selector (#321)

---
+++ src/customizations/volto/components/theme/LanguageSelector/LanguageSelector.jsx
@@ -0,0 +1,11 @@
+/**
+ * OVERRIDE LanguageSelector.js
+ * REASON: This theme uses a custom pre-@plone/components component
+ * SemanticUI-free located at the components folder.
+ * To override it, override the @kitconcept/volto-light-theme one instead of
+ * this one.
+ */
+
+import LanguageSelector from '../../../../../components/LanguageSelector/LanguageSelector';
+
+export default LanguageSelector;
