a080de678f6a9e4454966354bb1fb8ff88e34f46
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-02-13 17:19:48 +0100

    Fix missing key in header (#345)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,7 +47,9 @@ const InternetHeader = ({ pathname, siteLabel, token, siteAction }) => {
             {!token && <Anontools />}
             {siteAction &&
               siteAction.map((item) => (
-                <UniversalLink href={item.url}>{item.title}</UniversalLink>
+                <UniversalLink key={item.url} href={item.url}>
+                  {item.title}
+                </UniversalLink>
               ))}
           </div>
           {siteLabel && (
