551bb190f2047feaac99bd9348ec16af07aedfb9
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2024-05-01 03:57:41 -0600

    move block toolbar draft to independent components

---
+++ packages/volto-light-theme/src/components/BlockToolbar/ExperimentalBlockToolBar.jsx
@@ -0,0 +1,263 @@
+import { Group, Separator, Text } from 'react-aria-components';
+import dragSVG from '@plone/volto/icons/drag.svg';
+import { Icon } from '@plone/volto/components';
+import config from '@plone/volto/registry';
+import React, { useState, useCallback } from 'react';
+import { getCookieOptions, blockHasValue } from '@plone/volto/helpers';
+import { useCookies } from 'react-cookie';
+import { defineMessages } from 'react-intl';
+import isBoolean from 'lodash/isBoolean';
+import {
+  BlockToolbar,
+  Menu,
+  MenuItem,
+  BackgroundIcon,
+  MoreoptionsIcon,
+  SettingsIcon,
+  RowbeforeIcon,
+  RowafterIcon,
+  BinIcon,
+  Button,
+} from '@plone/components';
+import includes from 'lodash/includes';
+import BlockToolbarItem from './BlockToolbarItem';
+
+const ExperimentalToolbar = (props) => {
+  const { blockProps, draginfo, intl } = props;
+  const {
+    allowedBlocks,
+    block,
+    blocksConfig,
+    selected,
+    type,
+    onChangeBlock,
+    onDeleteBlock,
+    onInsertBlock,
+    onSelectBlock,
+    onMutateBlock,
+    onAddBlock,
+    data,
+    editable,
+    properties,
+    showBlockChooser,
+    navRoot,
+    contentType,
+    index,
+  } = blockProps;
+
+  const defaultToolbar = {
+    buttons: {
+      drag: [{ name: 'drag', icon: dragSVG, label: 'Drag', isMenuShape: true }],
+      styling: [
+        {
+          name: 'backgroundColor',
+          icon: <BackgroundIcon />,
+          label: 'Background Color',
+          isMenuShape: true,
+          options: config.settings.backgroundColors,
+        },
+      ],
+    },
+  };
+
+  const composeToolbar = (...toolbars) => {
+    const result = {};
+
+    const merge = (obj, key, value) => {
+      if (value !== undefined) {
+        if (typeof value === 'object' && !Array.isArray(value)) {
+          obj[key] = obj[key] || {};
+          Object.entries(value).forEach(([nestedKey, nestedValue]) => {
+            merge(obj[key], nestedKey, nestedValue);
+          });
+        } else if (Array.isArray(value)) {
+          if (obj[key] !== undefined) {
+            obj[key] = [...obj[key], ...value];
+          } else {
+            obj[key] = value;
+          }
+        }
+      }
+    };
+
+    toolbars.forEach((setting) => {
+      if (setting !== undefined) {
+        Object.entries(setting).forEach(([key, value]) => {
+          merge(result, key, value);
+        });
+      }
+    });
+    return result;
+  };
+
+  const toolbar = composeToolbar(defaultToolbar, blocksConfig?.[type]?.toolbar);
+
+  const [cookies, setCookie] = useCookies();
+
+  const [expanded, setExpanded] = useState(cookies.sidebar_expanded);
+
+  const messages = defineMessages({
+    delete: {
+      id: 'delete',
+      defaultMessage: 'delete',
+    },
+    settings: {
+      id: 'settings',
+      defaultMessage: 'settings',
+    },
+  });
+
+  const hideHandler = (data) => {
+    return (
+      !!data.fixed ||
+      (!config.experimental.addBlockButton.enabled &&
+        !(blockHasValue(data) && editable))
+    );
+  };
+  const visible = selected && !hideHandler(data);
+  const required = isBoolean(data.required)
+    ? data.required
+    : includes(config.blocks.requiredBlocks, type);
+
+  const resetFullSizeSidebar = useCallback(() => {
+    if (!expanded) {
+      const currentResizer = document.querySelector('#sidebar');
+      const sidebarContainer =
+        currentResizer.getElementsByClassName('sidebar-container')[0];
+      sidebarContainer.classList.remove('full-size');
+      sidebarContainer.classList.remove('no-toolbar');
+    }
+  }, [expanded]);
+
+  const sidebar = document.querySelector('#sidebar');
+  const sidebarContainer =
+    sidebar.getElementsByClassName('sidebar-container')[0];
+
+  const body = document.body;
+
+  const onToggleExpanded = () => {
+    if (!expanded) {
+      sidebarContainer.classList.remove('collapsed');
+      body.classList.remove('has-sidebar-collapsed');
+      body.classList.add('has-sidebar');
+    } else {
+      sidebarContainer.classList.add('collapsed');
+      body.classList.add('has-sidebar-collapsed');
+      body.classList.remove('has-sidebar');
+    }
+    setCookie('sidebar_expanded', !expanded, getCookieOptions());
+    setExpanded(!expanded);
+    resetFullSizeSidebar();
+  };
+
+  return (
+    <BlockToolbar
+      aria-label="Text formatting"
+      style={{
+        visibility: visible ? 'visible' : 'hidden',
+      }}
+    >
+      <div className="toolbar-container">
+        {toolbar?.buttons?.drag && (
+          <Group aria-label="Drag">
+            <div {...draginfo?.dragHandleProps} className="drag-handle-wrapper">
+              <Icon name={dragSVG} size="18px" />
+            </div>
+          </Group>
+        )}
+
+        {toolbar?.buttons?.textFormatting && (
+          <>
+            <Separator orientation="vertical" />
+            <Group aria-label="Text Formatting">
+              {toolbar?.buttons?.textFormatting.map((button) => {
+                return <BlockToolbarItem item={button} />;
+              })}
+            </Group>
+          </>
+        )}
+
+        {toolbar?.buttons?.styling && (
+          <>
+            <Separator orientation="vertical" />
+            <Group aria-label="Style">
+              {toolbar?.buttons?.styling.map((item) => {
+                return <BlockToolbarItem item={item} />;
+              })}
+            </Group>
+          </>
+        )}
+        {/* TODO: change More Options Group to use BlockToolbarItem component  */}
+        <Separator orientation="vertical" />
+        <Group aria-label="More Options">
+          <Menu button={<MoreoptionsIcon />}>
+            <MenuItem className={'button-wrapper'}>
+              <Button
+                icon
+                basic
+                onClick={() => onToggleExpanded()}
+                className="block-settings-button"
+                aria-label={intl.formatMessage(messages.settings)}
+              >
+                <SettingsIcon />
+                <Text slot="label">Settings</Text>
+              </Button>
+            </MenuItem>
+            <MenuItem className={'button-wrapper'}>
+              <Button
+                icon
+                basic
+                onClick={() =>
+                  onSelectBlock(
+                    onAddBlock(config.settings.defaultBlockType, index),
+                  )
+                }
+                className="add-block-button"
+                aria-label={intl.formatMessage(messages.delete)}
+              >
+                <RowbeforeIcon />
+                <Text slot="label">Insert block before</Text>
+              </Button>
+            </MenuItem>
+            <MenuItem className={'button-wrapper'}>
+              <Button
+                icon
+                basic
+                onClick={() =>
+                  onSelectBlock(
+                    onAddBlock(config.settings.defaultBlockType, index + 1),
+                  )
+                }
+                className="add-block-button"
+                aria-label={intl.formatMessage(messages.delete)}
+              >
+                <RowafterIcon />
+                <Text slot="label">Insert block after</Text>
+              </Button>
+            </MenuItem>
+
+            {selected && !required && editable && (
+              <>
+                <Separator />
+                <MenuItem className={'button-wrapper'}>
+                  <Button
+                    icon
+                    basic
+                    onClick={() => onDeleteBlock(block, true)}
+                    className="delete-block-button"
+                    aria-label={intl.formatMessage(messages.delete)}
+                  >
+                    <BinIcon />
+                    <Text slot="label">Remove block</Text>
+                  </Button>
+                </MenuItem>
+              </>
+            )}
+          </Menu>
+        </Group>
+      </div>
+    </BlockToolbar>
+  );
+};
+
+export default ExperimentalToolbar;
--- packages/volto-light-theme/src/customizations/volto/components/manage/Blocks/Block/EditBlockWrapper.jsx
+++ packages/volto-light-theme/src/customizations/volto/components/manage/Blocks/Block/EditBlockWrapper.jsx
@@ -6,41 +6,16 @@
  * DEVELOPER: @sneridagh
  */
 import React from 'react';
-import { Icon } from '@plone/volto/components';
 import {
   blockHasValue,
   buildStyleClassNamesFromData,
   buildStyleObjectFromData,
   buildStyleClassNamesExtenders,
 } from '@plone/volto/helpers';
-import dragSVG from '@plone/volto/icons/drag.svg';
-import { Button } from 'semantic-ui-react';
-import includes from 'lodash/includes';
-import isBoolean from 'lodash/isBoolean';
-import { defineMessages, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import cx from 'classnames';
 import config from '@plone/volto/registry';
-import {
-  BlockToolbar,
-  Menu,
-  MenuItem,
-  BoldIcon,
-  ItalicIcon,
-  LinkIcon,
-  MoreoptionsIcon,
-  SettingsIcon,
-  RowbeforeIcon,
-  RowafterIcon,
-  BinIcon,
-} from '@plone/components';
-import { Group, Separator, Text, ToggleButton } from 'react-aria-components';
-
-const messages = defineMessages({
-  delete: {
-    id: 'delete',
-    defaultMessage: 'delete',
-  },
-});
+import ExperimentalToolbar from '../../../../../../components/BlockToolbar/ExperimentalBlockToolBar';
 
 const EditBlockWrapper = (props) => {
   const hideHandler = (data) => {
@@ -51,7 +26,7 @@ const EditBlockWrapper = (props) => {
     );
   };
 
-  const { intl, blockProps, draginfo, children } = props;
+  const { blockProps, draginfo, children } = props;
   const {
     allowedBlocks,
     block,
@@ -72,11 +47,7 @@ const EditBlockWrapper = (props) => {
     index,
   } = blockProps;
 
-  const visible = selected && !hideHandler(data);
-
-  const required = isBoolean(data.required)
-    ? data.required
-    : includes(config.blocks.requiredBlocks, type);
+  const visible = selected && !hideHandler(blockProps.data);
 
   let classNames = buildStyleClassNamesFromData(data.styles);
   classNames = buildStyleClassNamesExtenders({
@@ -99,6 +70,10 @@ const EditBlockWrapper = (props) => {
 
   return (
     <div
+      onClick={() => {
+        onSelectBlock(block);
+      }}
+      aria-hidden="true"
       ref={draginfo.innerRef}
       {...styleMergedWithDragProps}
       // Right now, we can have the alignment information in the styles property or in the
@@ -115,98 +90,8 @@ const EditBlockWrapper = (props) => {
         },
       )}
     >
-      <BlockToolbar
-        aria-label="Text formatting"
-        style={{
-          visibility: visible ? 'visible' : 'hidden',
-        }}
-      >
-        <div className="toolbar-container">
-          <Group aria-label="Style">
-            <div {...draginfo.dragHandleProps} className="drag-handle-wrapper">
-              <Icon name={dragSVG} size="18px" />
-            </div>
-          </Group>
+      <ExperimentalToolbar aria-label="Toolbar" {...props} visible={visible} />
 
-          <Group aria-label="Style">
-            <ToggleButton aria-label="Bold">
-              <BoldIcon />
-            </ToggleButton>
-            <ToggleButton aria-label="Italic">
-              <ItalicIcon />
-            </ToggleButton>
-            <ToggleButton aria-label="Underline">
-              <LinkIcon />
-            </ToggleButton>
-          </Group>
-          <Separator orientation="vertical" />
-          <Menu button={<MoreoptionsIcon />}>
-            <MenuItem>
-              <SettingsIcon />
-              <Text slot="label">Settings</Text>
-            </MenuItem>
-            <MenuItem>
-              <Button
-                icon
-                basic
-                onClick={() =>
-                  props.blockProps.onSelectBlock(
-                    props.blockProps.onAddBlock(
-                      config.settings.defaultBlockType,
-                      index,
-                    ),
-                  )
-                }
-                className="add-block-button"
-                aria-label={intl.formatMessage(messages.delete)}
-                style={{ all: 'unset' }}
-              >
-                <RowbeforeIcon />
-                <Text slot="label">Insert block before</Text>
-              </Button>
-            </MenuItem>
-            <MenuItem>
-              <Button
-                icon
-                basic
-                onClick={() =>
-                  props.blockProps.onSelectBlock(
-                    props.blockProps.onAddBlock(
-                      config.settings.defaultBlockType,
-                      index + 1,
-                    ),
-                  )
-                }
-                className="add-block-button"
-                aria-label={intl.formatMessage(messages.delete)}
-                style={{ all: 'unset' }}
-              >
-                <RowafterIcon />
-                <Text slot="label">Insert block after</Text>
-              </Button>
-            </MenuItem>
-
-            {selected && !required && editable && (
-              <>
-                <Separator />
-                <MenuItem>
-                  <Button
-                    icon
-                    basic
-                    onClick={() => onDeleteBlock(block, true)}
-                    className="delete-block-button"
-                    aria-label={intl.formatMessage(messages.delete)}
-                    style={{ all: 'unset' }}
-                  >
-                    <BinIcon />
-                    <Text slot="label">Remove block</Text>
-                  </Button>
-                </MenuItem>
-              </>
-            )}
-          </Menu>
-        </div>
-      </BlockToolbar>
       <div style={{ position: 'relative' }}>
         {/* START CUSTOMIZATION */}
         <div className={cx('ui drag inner', { [type]: !isBlockModelv3 })}>
