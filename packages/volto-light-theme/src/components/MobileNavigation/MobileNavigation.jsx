1227c587a38d63154671cd09391f52e6f649fba9
Lena Daxenbichler <77293486+lenadax@users.noreply.github.com>
2024-06-21 11:19:06 +0200

    Customizable mobile navigation (#393)

--- packages/volto-light-theme/src/components/MobileNavigation/MobileNavigation.jsx
+++ packages/volto-light-theme/src/components/MobileNavigation/MobileNavigation.jsx
@@ -1,15 +1,17 @@
-import React, { useCallback } from 'react';
+import { useCallback, useState, useEffect, useRef } from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import cx from 'classnames';
 import { CSSTransition } from 'react-transition-group';
+import { doesNodeContainClick } from 'semantic-ui-react/dist/commonjs/lib';
 
 import config from '@plone/volto/registry';
 import { Icon, SearchWidget } from '@plone/volto/components';
 import { toBackendLang } from '@plone/volto/helpers';
 import arrowRightSVG from '@plone/volto/icons/right-key.svg';
 import arrowLeftSVG from '@plone/volto/icons/left-key.svg';
+import { MobileNavigationToggler } from './MobileNavigationToggler';
 import { MobileToolsFooter } from './MobileToolsFooter';
 
 const messages = defineMessages({
@@ -25,27 +27,106 @@ const messages = defineMessages({
     id: 'Search',
     defaultMessage: 'Search',
   },
+  back: {
+    id: 'Back',
+    defaultMessage: 'Back',
+  },
 });
 
-const MobileNavigation = (props) => {
-  const [menuState, setMenuState] = React.useState({
-    isMobileMenuOpen: false,
-    secondaryMenuOpened: null,
-    isSecondaryMobileMenuOpen: false,
-    tertiaryMenuOpened: null,
-    isTertiaryMobileMenuOpen: false,
-  });
+const MenuItem = ({
+  section,
+  level,
+  closeMenus,
+  handleLinkClicked,
+  resetToRoot,
+  pathname,
+}) => {
+  const [isSubMenuOpen, setSubMenuOpen] = useState(false);
 
-  const {
-    isMobileMenuOpen,
-    secondaryMenuOpened,
-    isSecondaryMobileMenuOpen,
-    tertiaryMenuOpened,
-    isTertiaryMobileMenuOpen,
-  } = menuState;
+  const openSubMenu = useCallback((e) => {
+    e.stopPropagation();
+    setSubMenuOpen(true);
+  }, []);
+
+  const closeSubMenu = useCallback((e) => {
+    e.stopPropagation();
+    setSubMenuOpen(false);
+  }, []);
+
+  // Reset submenus when history changes
+  useEffect(() => {
+    if (resetToRoot) {
+      setSubMenuOpen(false);
+    }
+  }, [resetToRoot]);
+
+  return (
+    <li className={section.url === pathname ? 'current' : ''}>
+      <Link
+        to={section.url === '' ? '/' : section.url}
+        onClick={(e) =>
+          handleLinkClicked(e, section, openSubMenu, closeSubMenu, level)
+        }
+      >
+        {section.nav_title || section.title}
+        {section.items.length > 0 && <Icon name={arrowRightSVG} />}
+      </Link>
+      <CSSTransition
+        in={isSubMenuOpen}
+        timeout={500}
+        classNames="menu-drawer"
+        unmountOnExit
+      >
+        <div className="menu-drawer subsection">
+          <div className="search-header">
+            <div className="search-wrapper">
+              <div className="search">
+                <SearchWidget />
+              </div>
+            </div>
+            <button onClick={closeSubMenu}>
+              <Icon name={arrowLeftSVG} size="60px" />
+              <span>
+                <FormattedMessage {...messages.back} />
+              </span>
+            </button>
+          </div>
+          <ul className="sections">
+            <li className="header">{section.nav_title || section.title}</li>
+            <li>
+              <Link
+                to={section.url === '' ? '/' : section.url}
+                onClick={closeMenus}
+              >
+                <FormattedMessage id="Overview" defaultMessage="Overview" />
+              </Link>
+            </li>
+            {section.items &&
+              section.items.map((subsection, index) => (
+                <MenuItem
+                  key={subsection.url}
+                  section={subsection}
+                  level={level + 1}
+                  closeMenus={closeMenus}
+                  handleLinkClicked={handleLinkClicked}
+                  resetToRoot={resetToRoot}
+                  pathname={pathname}
+                />
+              ))}
+          </ul>
+          <MobileToolsFooter />
+        </div>
+      </CSSTransition>
+    </li>
+  );
+};
+
+const MobileNavigation = (props) => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [resetToRoot, setResetToRoot] = useState(false);
   const { settings } = config;
   const intl = useIntl();
-  const menus = React.useRef(null);
+  const menus = useRef(null);
   const currentLang = useSelector((state) => state.intl.locale);
   const items = useSelector((state) => state.navigation.items || []);
   const history = useHistory();
@@ -55,94 +136,64 @@ const MobileNavigation = (props) => {
   const toggleMobileMenu = useCallback(() => {
     const body = document.getElementsByTagName('body')[0];
     body.classList.toggle('has-menu-open');
-
-    setMenuState((prevState) => ({
-      ...prevState,
-      isMobileMenuOpen: !prevState.isMobileMenuOpen,
-    }));
-  }, []);
-
-  const openSecondaryMenu = useCallback((e, index) => {
-    e.stopPropagation();
-
-    setMenuState((prevState) => ({
-      ...prevState,
-      secondaryMenuOpened: index,
-      isSecondaryMobileMenuOpen: true,
-    }));
-  }, []);
-
-  const closeSecondaryMenu = useCallback((e) => {
-    e.stopPropagation();
-    setMenuState((prevState) => ({
-      ...prevState,
-      isSecondaryMobileMenuOpen: false,
-      secondaryMenuOpened: null,
-    }));
-  }, []);
-
-  const openTertiaryMenu = useCallback((e, index) => {
-    e.stopPropagation();
-
-    setMenuState((prevState) => ({
-      ...prevState,
-      tertiaryMenuOpened: index,
-      isTertiaryMobileMenuOpen: true,
-    }));
-  }, []);
-
-  const closeTertiaryMenu = useCallback((e) => {
-    e.stopPropagation();
-
-    setMenuState((prevState) => ({
-      ...prevState,
-      isTertiaryMobileMenuOpen: false,
-      tertiaryMenuOpened: null,
-    }));
-  }, []);
+    setMobileMenuOpen((prev) => !prev);
+    if (!isMobileMenuOpen) {
+      setResetToRoot(false); // Disable reset to root when opening menu
+    }
+  }, [isMobileMenuOpen]);
 
   const closeMenus = useCallback((e) => {
     if (e && e.stopPropagation) {
       e.stopPropagation();
     }
-
-    setMenuState(() => ({
-      isSecondaryMobileMenuOpen: false,
-      isTertiaryMobileMenuOpen: false,
-      secondaryMenuOpened: null,
-      tertiaryMenuOpened: null,
-      isMobileMenuOpen: false,
-    }));
+    setMobileMenuOpen(false);
   }, []);
 
   const handleLinkClicked = useCallback(
-    (e, section, callback, index) => {
+    (e, section, openSubMenu, closeSubMenu, level) => {
       e.preventDefault();
       if (section.items.length > 0) {
-        callback(e, index);
+        openSubMenu(e);
       } else {
         history.push(section.url);
-        return closeMenus(e);
+        closeMenus(e);
+        setResetToRoot(true);
       }
     },
     [history, closeMenus],
   );
 
-  React.useEffect(() => {
-    const closeMenuOnHistoryChange = history.listen(() => closeMenus({}));
+  useEffect(() => {
+    const closeMenuOnHistoryChange = history.listen(() => {
+      closeMenus({});
+      setResetToRoot(true);
+    });
     return () => {
       closeMenuOnHistoryChange();
     };
   }, [history, closeMenus]);
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menus.current && !doesNodeContainClick(menus.current, e)) {
+        closeMenus(e);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside, false);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside, false);
+    };
+  }, [closeMenus]);
+
   return (
     <div className="mobile-nav mobile only tablet only" ref={menus}>
       <div className="hamburger-wrapper">
         <button
-          className={cx('hamburger hamburger--collapse', {
+          className={cx('hamburger-toggler hamburger-toggler--collapse', {
             'is-active': isMobileMenuOpen,
           })}
-          aria-expanded={isMobileMenuOpen ? true : false}
+          aria-expanded={isMobileMenuOpen}
           aria-label={
             isMobileMenuOpen
               ? intl.formatMessage(messages.closeMobileMenu)
@@ -156,9 +207,7 @@ const MobileNavigation = (props) => {
           type="button"
           onClick={toggleMobileMenu}
         >
-          <span className="hamburger-box">
-            <span className="hamburger-inner" />
-          </span>
+          <MobileNavigationToggler isMobileMenuOpen={isMobileMenuOpen} />
         </button>
       </div>
 
@@ -169,7 +218,7 @@ const MobileNavigation = (props) => {
       >
         <div className="menu-drawer">
           <div className="search-header">
-            <div className=" search-wrapper ">
+            <div className="search-wrapper">
               <div className="search">
                 <SearchWidget />
               </div>
@@ -190,169 +239,15 @@ const MobileNavigation = (props) => {
             </li>
             {items &&
               items.map((section, index) => (
-                <li
+                <MenuItem
                   key={section.url}
-                  className={section.url === props.pathname ? 'current' : ''}
-                >
-                  <Link
-                    to={section.url === '' ? '/' : section.url}
-                    onClick={(e) =>
-                      handleLinkClicked(e, section, openSecondaryMenu, index)
-                    }
-                  >
-                    {section.nav_title || section.title}
-                    {section.items.length > 0 && <Icon name={arrowRightSVG} />}
-                  </Link>
-                  <CSSTransition
-                    in={
-                      isSecondaryMobileMenuOpen && secondaryMenuOpened === index
-                    }
-                    timeout={500}
-                    classNames="menu-drawer"
-                    unmountOnExit
-                  >
-                    <div className="menu-drawer subsection">
-                      <div className="search-header">
-                        <div className=" search-wrapper ">
-                          <div className="search">
-                            <SearchWidget />
-                          </div>
-                        </div>
-
-                        <button onClick={(e) => closeSecondaryMenu(e)}>
-                          <Icon name={arrowLeftSVG} size="60px" />
-                          <span>
-                            <FormattedMessage id="Back" defaultMessage="Back" />
-                          </span>
-                        </button>
-                      </div>
-                      <ul className="sections">
-                        <li className="header">
-                          {section.nav_title || section.title}
-                        </li>
-                        <li>
-                          <Link
-                            to={section.url === '' ? '/' : section.url}
-                            onClick={closeMenus}
-                          >
-                            <FormattedMessage
-                              id="Overview"
-                              defaultMessage="Overview"
-                            />
-                          </Link>
-                        </li>
-
-                        {section.items &&
-                          section.items.map((subsection, index) => (
-                            <li
-                              key={subsection.url}
-                              className={
-                                subsection.url === props.pathname
-                                  ? 'current'
-                                  : ''
-                              }
-                            >
-                              <Link
-                                to={
-                                  subsection.url === '' ? '/' : subsection.url
-                                }
-                                onClick={(e) =>
-                                  handleLinkClicked(
-                                    e,
-                                    subsection,
-                                    openTertiaryMenu,
-                                    index,
-                                  )
-                                }
-                              >
-                                {subsection.nav_title || subsection.title}
-                                {subsection.items.length > 0 && (
-                                  <Icon name={arrowRightSVG} />
-                                )}
-                              </Link>
-                              <CSSTransition
-                                in={
-                                  isTertiaryMobileMenuOpen &&
-                                  tertiaryMenuOpened === index
-                                }
-                                timeout={500}
-                                classNames="menu-drawer"
-                                unmountOnExit
-                              >
-                                <div className="menu-drawer subsection">
-                                  <div className="search-header">
-                                    <div className=" search-wrapper ">
-                                      <div className="search">
-                                        <SearchWidget />
-                                      </div>
-                                    </div>
-
-                                    <button
-                                      onClick={(e) => closeTertiaryMenu(e)}
-                                    >
-                                      <Icon name={arrowLeftSVG} size="60px" />
-                                      <span>
-                                        <FormattedMessage
-                                          id="Back"
-                                          defaultMessage="Back"
-                                        />
-                                      </span>
-                                    </button>
-                                  </div>
-                                  <ul className="sections">
-                                    <li className="header">
-                                      {subsection.nav_title || subsection.title}
-                                    </li>
-                                    <li>
-                                      <Link
-                                        to={
-                                          subsection.url === ''
-                                            ? '/'
-                                            : subsection.url
-                                        }
-                                        onClick={closeMenus}
-                                      >
-                                        <FormattedMessage
-                                          id="Overview"
-                                          defaultMessage="Overview"
-                                        />
-                                      </Link>
-                                    </li>
-                                    {subsection.items &&
-                                      subsection.items.map(
-                                        (subsubsection, subindex) => (
-                                          <li
-                                            key={subsubsection.url}
-                                            className={cx('sub-section', {
-                                              'last-child':
-                                                subindex ===
-                                                subsection.items.length - 1,
-                                              current:
-                                                subsubsection.url ===
-                                                props.pathname,
-                                            })}
-                                          >
-                                            <Link
-                                              to={subsubsection.url}
-                                              onClick={closeMenus}
-                                            >
-                                              {subsubsection.nav_title ||
-                                                subsubsection.title}
-                                            </Link>
-                                          </li>
-                                        ),
-                                      )}
-                                  </ul>
-                                  <Footer />
-                                </div>
-                              </CSSTransition>
-                            </li>
-                          ))}
-                      </ul>
-                      <Footer />
-                    </div>
-                  </CSSTransition>
-                </li>
+                  section={section}
+                  level={0}
+                  closeMenus={closeMenus}
+                  handleLinkClicked={handleLinkClicked}
+                  resetToRoot={resetToRoot}
+                  pathname={props.pathname}
+                />
               ))}
           </ul>
           <Footer />
