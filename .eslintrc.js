54242b24f597974e3936b5f91efd5abdd6ebae1b
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-07-04 12:04:42 +0200

    Fix eslint config (#407)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const projectRootPath = __dirname;
+const AddonConfigurationRegistry = require('@plone/registry/src/addon-registry');
+
+let coreLocation;
+if (fs.existsSync(`${projectRootPath}/core`))
+  coreLocation = `${projectRootPath}/core`;
+else if (fs.existsSync(`${projectRootPath}/../../core`))
+  coreLocation = `${projectRootPath}/../../core`;
+
+const registry = new AddonConfigurationRegistry(
+  `${coreLocation}/packages/volto`,
+);
+
+// Extends ESlint configuration for adding the aliases to `src` directories in Volto addons
+const addonAliases = Object.keys(registry.packages).map((o) => [
+  o,
+  registry.packages[o].modulePath,
+]);
+
 module.exports = {
-  extends: './core/packages/volto/.eslintrc',
+  extends: `${coreLocation}/packages/volto/.eslintrc`,
   rules: {
     'import/no-unresolved': 1,
   },
@@ -7,10 +27,11 @@ module.exports = {
     'import/resolver': {
       alias: {
         map: [
-          ['@plone/volto', './core/packages/volto/src'],
-          ['@plone/volto-slate', './core/packages/volto-slate/src'],
-          ['@plone/registry', './core/packages/registry/src'],
+          ['@plone/volto', `${coreLocation}/packages/volto/src`],
+          ['@plone/volto-slate', `${coreLocation}/packages/volto-slate/src`],
+          ['@plone/registry', `${coreLocation}/packages/registry/src`],
           ['@kitconcept/volto-light-theme', './packages/volto-light-theme/src'],
+          ...addonAliases,
         ],
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       },
