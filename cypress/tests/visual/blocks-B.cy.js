8fe90b28564b9639aa5b56bb3510909bd8f316d0
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-10-22 17:36:09 +0200

    Split the blocks in two

--- cypress/tests/visual/blocks-B.cy.js
+++ cypress/tests/visual/blocks-B.cy.js
@@ -1,13 +1,6 @@
 import { visualTestBase } from './base';
 
 const PUBLIC_PAGES = [
-  '/block/heading-block',
-  '/block/button-block',
-  '/block/highlight-block',
-  '/block/image-block',
-  '/block/introduction-block',
-  '/block/separator-block',
-  '/block/teaser-block',
   '/block/table-block',
   '/block/text-block',
   '/block/video-block',
@@ -16,7 +9,7 @@ const PUBLIC_PAGES = [
   '/block/maps-block',
 ];
 
-const PRIVATE_PAGES = ['/block/button-block', '/block/block-accordion'];
+const PRIVATE_PAGES = ['/block/block-accordion'];
 
 describe('Blocks', () => {
   visualTestBase(PUBLIC_PAGES, PRIVATE_PAGES);
