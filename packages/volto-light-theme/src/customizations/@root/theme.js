e2e65d39237fa5d594fc591a9974001862f896de
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-10-23 12:57:25 +0200

    Add SemanticUI load under a layer

--- packages/volto-light-theme/src/customizations/@root/theme.js
+++ packages/volto-light-theme/src/customizations/@root/theme.js
@@ -1,7 +1,6 @@
 // This customization allows to enable the scape hatch to use
 // SCSS as a theme to override the SemanticUI theme.
-import 'semantic-ui-less/semantic.less';
-import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
+import './loadSemanticUI.less';
 import '@kitconcept/volto-light-theme/theme/main.scss';
 
 // Experimental SemanticUI behind a layer
