65ac406918de5fda0a094fdf2a274d76c1163640
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-11-07 12:48:36 +0100

    New docs (#423)
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

---
+++ docs/conf.py
@@ -0,0 +1,239 @@
+# Configuration file for the Sphinx documentation builder.
+# Plone Documentation build configuration file
+
+
+# -- Path setup --------------------------------------------------------------
+
+from datetime import datetime
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath("."))
+
+import os
+import json
+
+
+# -- Project information -----------------------------------------------------
+
+project = "@kitconcept/volto-light-theme documentation"
+copyright = "Plone Foundation"
+author = "kitconcept, GmbH"
+trademark_name = "Plone"
+now = datetime.now()
+year = str(now.year)
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+with open(os.path.join(os.path.abspath("."), "../package.json"), "r") as package_json:
+    data = package_json.read()
+
+version_from_package_json = json.loads(data)["version"]
+
+if version_from_package_json:
+    # The short X.Y version.
+    version = version_from_package_json
+    # The full version, including alpha/beta/rc tags.
+    release = version_from_package_json
+else:
+    version = "6.0.0"
+    release = "6.0.0"
+
+
+# -- General configuration ----------------------------------------------------
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ["_templates"]
+
+# Add any Sphinx extension module names here, as strings.
+# They can be extensions coming with Sphinx (named "sphinx.ext.*")
+# or your custom ones.
+extensions = [
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinxext.opengraph",
+]
+
+
+# If true, the Docutils Smart Quotes transform, originally based on SmartyPants
+# (limited to English) and currently applying to many languages, will be used
+# to convert quotes and dashes to typographically correct entities.
+# Note to maintainers: setting this to `True` will cause contractions and
+# hyphenated words to be marked as misspelled by spellchecker.
+smartquotes = False
+
+# The name of the Pygments (syntax highlighting) style to use.
+# pygments_style = "sphinx.pygments_styles.PyramidStyle"
+pygments_style = "sphinx"
+
+# Options for the linkcheck builder
+# Ignore localhost
+linkcheck_ignore = [
+    r"http://127.0.0.1",
+    r"http://localhost",
+    # Ignore links to static files
+    r"/_static",
+    # Ignore pages that require authentication
+    r"https://github.com/kitconcept/volto-light-theme/issues/new",  # requires auth
+    # Ignore github.com pages with anchors
+    r"https://github.com/.*#.*",
+    # Ignore other specific anchors
+]
+linkcheck_anchors = True
+linkcheck_timeout = 5
+linkcheck_retries = 1
+
+# The suffix of source filenames.
+source_suffix = {
+    ".md": "markdown",
+    ".bugfix": "markdown",
+    ".breaking": "markdown",
+    ".documentation": "markdown",
+    ".feature": "markdown",
+    ".internal": "markdown",
+}
+
+# The master toctree document.
+master_doc = "index"
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+suppress_warnings = [
+    # "toc.excluded",  # Suppress `WARNING: document isn't included in any toctree`
+    "toc.not_readable",  # Suppress `WARNING: toctree contains reference to nonexisting document 'news*'`
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "plone_sphinx_theme"
+html_logo = "_static/logo.svg"
+html_favicon = "_static/favicon.ico"
+html_theme_options = {
+    "article_header_start": ["toggle-primary-sidebar"],
+    "extra_footer": """<p>The text and illustrations in this website are licensed by the Plone Foundation under a Creative Commons Attribution 4.0 International license. Plone and the Plone® logo are registered trademarks of the Plone Foundation, registered in the United States and other countries. For guidelines on the permitted uses of the Plone trademarks, see <a href="https://plone.org/foundation/logo">https://plone.org/foundation/logo</a>. All other trademarks are owned by their respective owners.</p>
+<p>Pull request previews by <a href="https://readthedocs.org/" target="_blank">Read the Docs</a>.</p>""",
+    "footer_end": ["version.html"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/plone/volto",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css",
+            },
+        },
+        {
+            "name": "Mastodon",
+            "url": "https://plone.social/@plone",
+            "icon": "fa-brands fa-mastodon",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css",
+            },
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/plone",
+            "icon": "fa-brands fa-square-twitter",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css",
+            },
+        },
+    ],
+    "logo": {
+        "text": "@kitconcept/volto-light-theme Documentation",
+    },
+    "navigation_with_keys": True,
+    "path_to_docs": "docs",
+    "repository_branch": "main",
+    "repository_url": "https://github.com/kitconcept/volto-light-theme/tree/main/",
+    "search_bar_text": "Search",  # TODO: Confirm usage of search_bar_text in plone-sphinx-theme
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+}
+
+# Announce that we have an opensearch plugin
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_use_opensearch
+html_use_opensearch = "https://volto-light-theme.readthedocs.io/"  # TODO: Confirm usage of opensearch in theme
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = "%(project)s v%(release)s" % {"project": project, "release": release}
+
+html_css_files = ["custom.css", ("print.css", {"media": "print"})]
+
+# If false, no index is generated.
+html_use_index = True
+
+html_extra_path = [
+    "robots.txt",
+]
+
+html_static_path = [
+    "_static",
+]
+
+
+# -- Options for MyST markdown conversion to HTML -----------------------------
+
+# For more information see:
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = [
+    "deflist",  # Support definition lists.
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#definition-lists
+    "linkify",  # Identify "bare" web URLs and add hyperlinks.
+    "colon_fence",  # You can also use ::: delimiters to denote code fences,\
+    #  instead of ```.
+    "html_image",  # For inline images. See https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#html-images
+]
+
+
+# -- Intersphinx configuration ----------------------------------
+
+# This extension can generate automatic links to the documentation of objects
+# in other projects. Usage is simple: whenever Sphinx encounters a
+# cross-reference that has no matching target in the current documentation set,
+# it looks for targets in the documentation sets configured in
+# intersphinx_mapping. A reference like :py:class:`zipfile.ZipFile` can then
+# linkto the Python documentation for the ZipFile class, without you having to
+# specify where it is located exactly.
+#
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+#
+intersphinx_mapping = {
+    "plone": ("https://6.docs.plone.org/", None),
+}
+
+
+# -- OpenGraph configuration ----------------------------------
+
+ogp_site_url = "https://volto-light-theme.readthedocs.io/"
+ogp_description_length = 200
+ogp_image = (
+    "https://volto-light-theme.readthedocs.io/en/latest/_static/Plone_logo_square.png"
+)
+ogp_site_name = "@kitconcept/volto-light-theme Documentation"
+ogp_type = "website"
+ogp_custom_meta_tags = [
+    '<meta property="og:locale" content="en_US" />',
+]
---
+++ docs/upgrade-guide.md
@@ -0,0 +1,120 @@
+---
+myst:
+  html_meta:
+    "description": "Volto Light Theme upgrade guide"
+    "property=og:description": "Volto Light Theme upgrade guide"
+    "property=og:title": "Upgrade guide"
+    "keywords": "Volto Light Theme, upgrade guide"
+---
+
+# Upgrade guide
+
+## volto-light-theme 6.0.0
+
+### Color definitions
+
+The VLT has migrated to use the standardized color definitions in Volto.
+These new definitions use CSS properties that are injected at runtime in the right places, so your CSS can adapt to use them in a generic way.
+The resultant CSS is simpler, and there's no need to define class names for each color definition.
+Read more about them in: https://6.docs.plone.org/volto/development/color-picker-widget.html?highlight=color#custom-css-properties-as-color-definitions
+
+The new color definitions are no longer hardcoded, and now they are stored in `config.settings.backgroundColors`.
+
+```ts
+config.settings.backgroundColors = [
+  {
+    style: {
+      '--background-color': 'transparent',
+    },
+    name: 'transparent',
+    label: 'Transparent',
+  },
+  {
+    style: {
+      '--background-color': '#ecebeb',
+    },
+    name: 'grey',
+    label: 'Grey',
+  },
+];
+```
+
+```css
+.block.teaser {
+  background-color: var(---background-color, #fff)
+}
+```
+
+If you extended or modified the default background colors in VLT, you need to move the colors that you are using accordingly to the new definitions.
+Once the new definitions are in place, there is a content reducer transform in place that will convert the blocks on the fly.
+Whenever you save an object, the object will be saved using the new styling.
+Make sure that you use the same `name` as before when you update your color definitions.
+
+
+### Block widths
+
+VLT has started to use the standard block width definition as well.
+It's stored in `config.settings.blocksWidths`.
+It uses a new widget component that would be ported to Volto core as soon as it's ready.
+This component saves the value of the custom CSS property `--block-width` as a StyleWrapper value, so it can be used later when the StyleWrapper injects it in the block (or component) markup.
+
+
+## volto-light-theme 5.0.0
+
+The requirements for VLT have changed:
+
+| VLT version | Volto version |
+|-------------|---------------|
+|   3.x.x  |   >= Volto 17.0.0-alpha.16  |
+|   4.x.x  |   < Volto 17.18.0  |
+|   5.x.x  |   >= Volto 17.18.0 or >=Volto 18.0.0-alpha.36  |
+
+
+## volto-light-theme 4.0.0
+
+The tabbing order in the top header was fixed for accessibility concerns.
+It modifies the underlying HTML, moving from the top header to the bottom, and modifies the CSS to adjust.
+
+The `MobileNavigation` component was updated to be more easily customizable.
+The component can now handle infinite navigation depth instead of only three levels, if configured to do so.
+The Burger Menu can now be easily customized by overriding the new {file}`MobileNavigationToggler.jsx` file.
+
+
+## volto-light-theme 3.0.0
+
+### Blocks background colors go full width
+
+The background colors previously snapped to 1440px.
+From 3.0.0-alpha.0 onward, this changed to be unconstrained by default, and expands to the end of the horizontal viewport.
+
+
+### Upgraded support for volto-slider-block 6.0.0
+
+VLT upgraded the dependency on `@kitconcept/volto-slider-block` to use `6.0.0`.
+
+This is a drop-in replacement, so no action is required for the existing slider blocks you may already have in your sites.
+However, the CSS classes of the structural slider block elements changed in this version.
+The inner (visible objects) CSS classes remain unchanged.
+If you have customized them in your project, you may have to update them, although the structural class names are rarely customized aside from vertical spacing properties.
+They are mapped 1:1 with the previous ones, following this table correspondence:
+
+| Old className   | New className    |
+| --------------- | ---------------- |
+| slick-slider    | slider-wrapper   |
+| slick-list      | slider-viewport  |
+| slick-track     | slider-container |
+| slick-slide     | slider-slide     |
+| slick-arrow     | slider-button    |
+| slick-prev      | slider-button-prev |
+| slick-next      | slider-slide-next  |
+| slick-next      | slider-slide-next  |
+| slick-dots      | slider-dots      |
+| slick-dot       | slider-dot       |
+
+For more information, check the https://github.com/kitconcept/volto-slider-block/blob/main/README.md and [#288](https://github.com/kitconcept/volto-light-theme/pull/288).
+
+
+### Language switcher shows the two-letter language code
+
+The default in Volto is to show the full name of the language in the language switcher.
+VLT now shows only the two-letter language code.
