0f85c54e71be70494f22c5cd02d4ce5ad75295c6
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-11-11 09:59:36 +0100

    Move leaked file to this PR

--- packages/volto-light-theme/src/customizations/@root/theme.js
+++ packages/volto-light-theme/src/customizations/@root/theme.js
@@ -1,4 +1,5 @@
 // This customization allows to enable the scape hatch to use
 // SCSS as a theme to override the SemanticUI theme.
+// Experimental SemanticUI behind a layer
 import './loadSemanticUI.less';
 import '@kitconcept/volto-light-theme/theme/main.scss';
