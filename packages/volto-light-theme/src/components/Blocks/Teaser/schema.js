01162ab46e78bf7d414b3d1564454b29194151cb
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-12-13 10:28:29 +0100

    Remove aspect-ratio by default in teaser and images (#448)

--- packages/volto-light-theme/src/components/Blocks/Teaser/schema.js
+++ packages/volto-light-theme/src/components/Blocks/Teaser/schema.js
@@ -1,20 +1,6 @@
 export const teaserSchemaEnhancer = ({ schema, formData, intl }) => {
   schema.properties.href.selectedItemAttrs.push('getRemoteUrl');
 
-  schema.properties.styles.schema.fieldsets[0].fields = [
-    ...schema.properties.styles.schema.fieldsets[0].fields,
-    '--image-aspect-ratio',
-  ];
-
-  schema.properties.styles.schema.properties['--image-aspect-ratio'] = {
-    widget: 'select',
-    title: 'Aspect Ratio',
-    choices: [
-      ['1', '1:1'],
-      ['16 / 9', '16/9'],
-    ],
-  };
-
   return schema;
 };
 
