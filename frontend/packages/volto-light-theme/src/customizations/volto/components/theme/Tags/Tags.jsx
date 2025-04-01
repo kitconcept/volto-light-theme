636723eab03bcf81d79c300b8d4baedad186906c
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-04-01 16:46:40 +0200

    Fixed Tags component container (#517)

---
+++ frontend/packages/volto-light-theme/src/customizations/volto/components/theme/Tags/Tags.jsx
@@ -0,0 +1,11 @@
+/**
+ * OVERRIDE Tags.jsx
+ * REASON: This theme uses a custom pre-@plone/components component
+ * SemanticUI-free located at the components folder.
+ * To override it, override the @kitconcept/volto-light-theme one instead of
+ * this one.
+ */
+
+import Tags from '../../../../../components/Tags/Tags';
+
+export default Tags;
