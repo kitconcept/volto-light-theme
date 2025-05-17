6796cfa88185ac26d69bf5afae217c36752e7adb
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-05-17 09:00:45 +0200

    Added the logos block and the carousel (#550)

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,10 +1,13 @@
 const addons = [
   '@plonegovbr/volto-social-media',
   '@eeacms/volto-accordion-block',
+  '@kitconcept/volto-banner-block',
   '@kitconcept/volto-button-block',
+  '@kitconcept/volto-carousel-block',
   '@kitconcept/volto-heading-block',
   '@kitconcept/volto-highlight-block',
   '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-logos-block',
   '@kitconcept/volto-separator-block',
   '@kitconcept/volto-slider-block',
   '@kitconcept/volto-banner-block',
