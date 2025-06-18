c8a83040bef2a79a77df217137dfd76773abc4f5
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-06-18 16:03:21 +0200

    Fixpersonteaserselectionfirsttime (#586)

--- frontend/packages/volto-light-theme/src/components/Blocks/Teaser/schema.js
+++ frontend/packages/volto-light-theme/src/components/Blocks/Teaser/schema.js
@@ -3,6 +3,10 @@ export const teaserSchemaEnhancer = ({ schema, formData, intl }) => {
   schema.properties.href.selectedItemAttrs.push('effective');
   schema.properties.href.selectedItemAttrs.push('start');
   schema.properties.href.selectedItemAttrs.push('end');
+  schema.properties.href.selectedItemAttrs.push('description');
+  schema.properties.href.selectedItemAttrs.push('contact_email');
+  schema.properties.href.selectedItemAttrs.push('contact_phone');
+  schema.properties.href.selectedItemAttrs.push('contact_room');
 
   return schema;
 };
