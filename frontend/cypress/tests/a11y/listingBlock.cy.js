9db05775bd47d6cab3fc5a3f7dc038a49bb4f3e9
iFlameing <ialokkumarsingh0@gmail.com>
2025-07-14 15:34:41 +0530

    fix a11y test

--- frontend/cypress/tests/a11y/listingBlock.cy.js
+++ frontend/cypress/tests/a11y/listingBlock.cy.js
@@ -17,6 +17,7 @@ describe('a11y tests', () => {
     cy.configureAxe({
       // Disabling 'image-alt'
       // semantic-ui-react's Embed doesn't include an alt tag for the placeholder image
+      // Disabling the link-name rule because in card component we are not giving link any text.
       rules: [
         {
           id: 'image-alt',
@@ -24,6 +25,10 @@ describe('a11y tests', () => {
         },
         {
           id: 'nested-interactive',
+          enabled: false,
+        },
+        {
+          id: 'link-name',
           enabled: false,
         },
       ],
