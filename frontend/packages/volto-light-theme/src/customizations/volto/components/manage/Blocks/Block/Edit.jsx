670bcc81bd690797a3b5a32c708d9d5bfc0e2733
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-07-17 09:44:31 +0200

    Block Model 3 structural changes as opt-in in VLT6 (#532)
    Co-authored-by: Dante Álvarez <89805481+danalvrz@users.noreply.github.com>

---
+++ frontend/packages/volto-light-theme/src/customizations/volto/components/manage/Blocks/Block/Edit.jsx
@@ -0,0 +1,10 @@
+/**
+ * OVERRIDE: Edit.jsx
+ * REASON: Adding BlockModelv3 wrappers
+ * DATE: 2024-02-01
+ * DEVELOPER: @sneridagh
+ */
+
+import Edit from '../../../../../../components/Blocks/Block/Edit';
+
+export default Edit;
