bb802f9049038fea217e54e69ae5eba0b9eeefca
Tishasoumya-02 <tishasoumya@gmail.com>
2025-07-18 14:31:29 +0530

    docs for customization

--- frontend/packages/volto-light-theme/src/customizations/@kitconcept/volto-slider-block/components/View.jsx
+++ frontend/packages/volto-light-theme/src/customizations/@kitconcept/volto-slider-block/components/View.jsx
@@ -1,3 +1,12 @@
+/**
+ * OVERRIDE View.jsx
+ * REASON: Adding variants.
+ * FILE: https://github.com/kitconcept/volto-slider-block/blob/432ff2a40fc29c98890c4ac81d5a85f52488b318/packages/volto-slider-block/src/components/View.jsx
+ * DATE: 2025-07-18
+ * DEVELOPER: @Tishasoumya-02
+ */
+
+
 import View from '../../../../components/Blocks/Slider/View';
 
 export default View;
