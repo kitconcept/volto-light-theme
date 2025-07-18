75aa59b2337a10244b394c6af22b9b4e65e1839f
Alok Kumar <ialokkumarsingh0@gmail.com>
2025-07-18 18:21:30 +0530

    Add the query support, fix the daterangequery with facet one and add … (#609)

--- frontend/cypress/tests/main/blocks-calendar.cy.js
+++ frontend/cypress/tests/main/blocks-calendar.cy.js
@@ -5,6 +5,8 @@ describe('Event Calendar Block Tests', () => {
     cy.intercept('GET', `/**/*?expand*`).as('content');
     cy.intercept('GET', '/**/Document').as('schema');
     cy.intercept('GET', '**/@querystring-search**').as('querySearch');
+    cy.intercept('PATCH', '/**/my-page').as('save');
+
     const now = new Date();
     startDate = new Date(now);
     startDate.setMonth(startDate.getMonth() + 1);
@@ -26,6 +28,16 @@ describe('Event Calendar Block Tests', () => {
       contentType: 'Event',
       contentId: 'my-first-event',
       contentTitle: 'My First Event',
+      bodyModifier(body) {
+        body.start = formatDate(eventStart);
+        body.end = formatDate(eventEnd);
+        return body;
+      },
+    });
+    cy.createContent({
+      contentType: 'Event',
+      contentId: 'my-search-event',
+      contentTitle: 'Search Event',
       bodyModifier(body) {
         body.start = formatDate(eventStart);
         body.end = formatDate(eventEnd);
@@ -102,6 +114,10 @@ describe('Event Calendar Block Tests', () => {
   });
 
   it('Add Event Calendar block and test facet', () => {
+    cy.setWorkflow({
+      path: 'my-first-event',
+      review_state: 'publish',
+    });
     // Adding new event calendar block and facet
     cy.addNewBlock('event');
     cy.get('#sidebar-properties .field-wrapper-facets button')
@@ -132,9 +148,270 @@ describe('Event Calendar Block Tests', () => {
     cy.wait('@querySearch');
     cy.get('.row.template-container')
       .findByText('My First Event')
-      .should('exist');
+      .should('not.exist');
     cy.get('.row.template-container')
       .findByText('Second Event')
       .should('exist');
+  });
+
+  it('Add Event Calendar block and test query field', () => {
+    cy.setWorkflow({
+      path: 'my-first-event',
+      review_state: 'publish',
+    });
+    cy.addNewBlock('event');
+    cy.get('.sidebar-container .tabs-wrapper .menu .item')
+      .contains('Block')
+      .click();
+    cy.get('.querystring-widget .fields').contains('Add criteria').click();
+    cy.get(
+      '.querystring-widget .fields:first-of-type .field:first-of-type .react-select__menu .react-select__option',
+    )
+      .contains('Review state')
+      .click();
+
+    //insert Page
+    cy.get('.querystring-widget .fields:first-of-type > .field').click();
+    cy.get(
+      '.querystring-widget .fields:first-of-type > .field .react-select__menu .react-select__option',
+    )
+      .contains('Private')
+      .click();
+    cy.get('#toolbar-save').click();
+    cy.wait('@content');
+    cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
+    cy.wait('@querySearch');
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('not.exist');
+    cy.get('.row.template-container')
+      .findByText('Second Event')
+      .should('exist');
+  });
+
+  it('Respect batching,limits and pagination', () => {
+    cy.createContent({
+      contentType: 'Event',
+      contentId: 'my-folder',
+      contentTitle: 'My Folder',
+      path: 'my-page',
+    });
+    cy.createContent({
+      contentType: 'Event',
+      contentId: 'my-folder2',
+      contentTitle: 'My Folder 2',
+      path: 'my-page',
+    });
+    cy.createContent({
+      contentType: 'Event',
+      contentId: 'my-folder3',
+      contentTitle: 'My Folder 3',
+      path: 'my-page',
+    });
+    cy.addNewBlock('event');
+    cy.configureListingWith('Event');
+    cy.get('#field-limit-3-query').type('2');
+
+    //save
+    cy.get('#toolbar-save').click();
+    cy.wait('@save');
+    cy.wait('@content');
+
+    cy.get('.card-listing').should(($els) => {
+      expect($els).to.have.length(2);
+    });
+
+    cy.navigate('/my-page/edit');
+    cy.wait('@schema');
+    cy.get('.block-editor-eventCalendar').click();
+    cy.get('#field-limit-3-query').clear().type('0');
+    cy.get('#field-b_size-4-query').type('2');
+    cy.get('#toolbar-save').click();
+    cy.wait('@save');
+    cy.wait('@content');
+    cy.get('.card-listing').should(($els) => {
+      expect($els).to.have.length(2);
+    });
+    cy.get('.ui.pagination.menu a[value="2"]').first().click({ force: true });
+    cy.wait('@querySearch');
+    cy.get('.card-listing').should(($els) => {
+      expect($els).to.have.length(2);
+    });
+  });
+
+  it('Test the daterange with facet and input', () => {
+    // Adding new event calendar block and setting the two event content published.
+    cy.setWorkflow({
+      path: 'my-first-event',
+      review_state: 'publish',
+    });
+    cy.setWorkflow({
+      path: 'my-search-event',
+      review_state: 'publish',
+    });
+    cy.addNewBlock('event');
+
+    // setting the facet
+    cy.get('#sidebar-properties .field-wrapper-facets button')
+      .findByText('Add Facet')
+      .click({ force: true });
+    cy.get('[id^="field-field-1-"] .react-select__value-container').click();
+    cy.get('.react-select__option').contains('Review state').click();
+    cy.get('#toolbar-save').click();
+    cy.wait('@content');
+    cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
+
+    // Now we are going to select facet if it is public or private
+    cy.get('.facets .react-select-container ').click();
+    cy.get('.react-select__option').contains('Published').click();
+    cy.wait('@querySearch');
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('exist');
+    cy.get('.row.template-container')
+      .findByText('Second Event')
+      .should('not.exist');
+
+    // Now we are going to set the date range.
+    cy.get('.react-aria-Group [slot="start"] [data-type="day"]')
+      .focus()
+      .type(startDate.getDate().toString());
+    cy.get('.react-aria-Group [slot="start"] [data-type="month"]')
+      .focus()
+      .type((startDate.getMonth() + 1).toString());
+
+    cy.get('.react-aria-Group [slot="start"] [data-type="year"]')
+      .focus()
+      .type(startDate.getFullYear().toString());
+
+    cy.get('.react-aria-Group [slot="end"] [data-type="day"]')
+      .focus()
+      .type(endDate.getDate().toString());
+
+    cy.get('.react-aria-Group [slot="end"] [data-type="month"]')
+      .focus()
+      .type((endDate.getMonth() + 1).toString());
+
+    cy.get('.react-aria-Group [slot="end"] [data-type="year"]')
+      .focus()
+      .type(endDate.getFullYear().toString());
+
+    cy.wait('@querySearch');
+
+    // After setting the date range, we should see the first event.
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('exist');
+    cy.get('.row.template-container')
+      .findByText('Second Event')
+      .should('not.exist');
+
+    // Now we are going to search the event.
+    cy.get('.first-row .search-input input').type('Search Event');
+
+    cy.wait('@querySearch');
+
+    // After searching, we should see the searched event.
+    cy.get('.row.template-container')
+      .findByText('Search Event')
+      .should('exist');
+    // We should not see the first event.
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('not.exist');
+    cy.get('.first-row .search-input input').clear();
+    // Now changing to the private review state
+    cy.wait(1000); // This is necessary to wait for re-clicking the facet.
+    cy.get('.facets .react-select-container .react-select__control ').click();
+    cy.get('.react-select__option').contains('Private').click();
+    cy.wait('@querySearch');
+
+    // After changing the review state to private, we should not see the first event.
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('not.exist');
+    cy.get('.row.template-container')
+      .findByText('Second Event')
+      .should('not.exist');
+
+    // Now we are going to reset the date range.
+    cy.get('.react-aria-DateRangePicker .reset-date-range').click();
+    cy.wait('@querySearch');
+    // Since we reset the date range and facet is still private, we should not see the first event.
+    cy.get('.row.template-container')
+      .findByText('My First Event')
+      .should('not.exist');
+  });
+
+  it('EventCalendar block - Test Criteria: short-name', () => {
+    cy.clearSlateTitle().type(
+      'EventCalendar block - Test Criteria: short-name',
+    );
+
+    //add eventCalendar block
+    cy.addNewBlock('event');
+
+    //********  add short-name criteria filter
+    cy.get('.sidebar-container .tabs-wrapper .menu .item')
+      .contains('Block')
+      .click();
+    cy.get('.querystring-widget .fields').contains('Add criteria').click();
+    cy.get(
+      '.querystring-widget .fields:first-of-type .field:first-of-type .react-select__menu .react-select__option',
+    )
+      .contains('Short name (id)')
+      .click();
+    //short-name is..
+    cy.get(
+      '.querystring-widget .fields:first-of-type .main-fields-wrapper .field:last-of-type',
+    ).click();
+    cy.get(
+      '.querystring-widget .fields:first-of-type .main-fields-wrapper .field:last-of-type .react-select__menu .react-select__option',
+    )
+      .contains('Is')
+      .click();
+    //insert short name
+    cy.get('.querystring-widget .fields:first-of-type > .field input')
+      .clear()
+      .type('my-first-event');
+
+    //before save, vrify if in list there's a page with id my-page-test
+    cy.get(`.block.eventCalendar .card-listing:first-of-type .title`).contains(
+      'My First Event',
+    );
+    //before save, verify if in list there isn't the Event with title Search event
+    cy.get(`.block.eventCalendar .card-listing .title`)
+      .contains('Search Event')
+      .should('not.exist');
+
+    //save
+    cy.get('#toolbar-save').click();
+    cy.wait('@save');
+    cy.wait('@content');
+
+    cy.get('#page-document .card-listing:first-of-type').contains(
+      'My First Event',
+    );
+    cy.get('#page-document .card-listing:first-of-type a').should(
+      'have.attr',
+      'href',
+      '/my-first-event',
+    );
+  });
+
+  it('Event calendar show single date for same day event and double for different', () => {
+    //add eventCalendar block
+    cy.addNewBlock('event');
+    cy.get('#toolbar-save').click();
+    cy.wait('@content');
+    cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
+    cy.get('.card-listing:first-of-type .image-wrapper .date-inset').should(
+      'not.have.class',
+      'has-end-date',
+    );
+    cy.get('.card-listing:nth-child(2) .image-wrapper .date-inset').should(
+      'have.class',
+      'has-end-date',
+    );
   });
 });
