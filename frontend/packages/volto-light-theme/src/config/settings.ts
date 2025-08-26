73e3ac57996905e132ad30fae06fc47544074d18
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-08-26 12:25:55 +0200

    Move ConfigInjector code to k.intranet (#629)

--- frontend/packages/volto-light-theme/src/config/settings.ts
+++ frontend/packages/volto-light-theme/src/config/settings.ts
@@ -21,7 +21,7 @@ type apiExpanderInherit = {
 
 export default function install(config: ConfigType) {
   const EXPANDERS_INHERIT_BEHAVIORS =
-    'voltolighttheme.header,voltolighttheme.theme,voltolighttheme.footer,kitconcept.footer,kitconcept.sticky_menu,kitconcept.blocks.config';
+    'voltolighttheme.header,voltolighttheme.theme,voltolighttheme.footer,kitconcept.footer,kitconcept.sticky_menu';
   config.settings.enableAutoBlockGroupingByBackgroundColor = true;
   config.settings.navDepth = 3;
   config.settings.slate.useLinkedHeadings = false;
