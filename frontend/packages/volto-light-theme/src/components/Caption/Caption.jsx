886af143c7079fb340c3c5faab1de3187ec107e7
jackahl <kahl@kitconcept.com>
2025-09-10 12:12:49 +0200

    switch image caption headings to strong tag

--- frontend/packages/volto-light-theme/src/components/Caption/Caption.jsx
+++ frontend/packages/volto-light-theme/src/components/Caption/Caption.jsx
@@ -18,7 +18,7 @@ const Caption = ({ as = 'figcaption', title, description, credit }) => {
   const As = as;
   return (
     <As>
-      {title && <h5 className="title">{title}</h5>}
+      {title && <strong className="title">{title}</strong>}
       {description && (
         <div className="description">
           {description.split('\n').map((line, index) => (
