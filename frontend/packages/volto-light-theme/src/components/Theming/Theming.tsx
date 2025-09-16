5ace6cd30551cc220293cdee977d08b592a5be46
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-09-16 17:41:52 +0200

    Fixed theming colors injection on add/edit view (#660)

--- frontend/packages/volto-light-theme/src/components/Theming/Theming.tsx
+++ frontend/packages/volto-light-theme/src/components/Theming/Theming.tsx
@@ -1,8 +1,10 @@
 import Helmet from '@plone/volto/helpers/Helmet/Helmet';
-import type { Content } from '@plone/types';
+import type { Content, GetTypeResponse } from '@plone/types';
 import { useSelector } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
 import type { SiteThemeSettings } from '../../types';
+import { useLocation } from 'react-router-dom';
+import { flattenToAppURL, getBaseUrl } from '@plone/volto/helpers/Url/Url';
 
 type FormState = {
   content: {
@@ -10,6 +12,9 @@ type FormState = {
   };
   form: {
     global: Content;
+  };
+  schema: {
+    schema: GetTypeResponse;
   };
 };
 
@@ -24,17 +29,34 @@ function buildStyleTag(content: Partial<Content>, colors: SiteThemeSettings) {
 }
 
 const Theming = ({ content }: { content: Content }) => {
+  const location = useLocation();
   const colorSettings =
     content?.['@components']?.inherit?.['voltolighttheme.theme']?.data;
   const formData = useSelector<FormState, Content>(
     (state) => state.form.global,
   );
+  const schema = useSelector<FormState, GetTypeResponse>(
+    (state) => state.schema.schema,
+  );
 
-  const liveContent = colorSettings
-    ? !isEmpty(formData)
-      ? formData
-      : colorSettings
-    : {};
+  // Check if content has all the keys of the VLT theme behavior
+  const hasVLTthemeBehavior = Object.keys(colorSettings || {}).every((key) =>
+    Object.prototype.hasOwnProperty.call(schema?.properties || {}, key),
+  );
+
+  const isForm = !isEmpty(formData);
+  const addingFromInherited =
+    location.pathname.endsWith('/add') &&
+    flattenToAppURL(
+      content?.['@components']?.inherit?.['voltolighttheme.theme'].from['@id'],
+    ) === getBaseUrl(location.pathname);
+
+  let liveContent;
+  if (hasVLTthemeBehavior && !addingFromInherited) {
+    liveContent = colorSettings ? (isForm ? formData : colorSettings) : {};
+  } else {
+    liveContent = colorSettings;
+  }
 
   return (
     <>
