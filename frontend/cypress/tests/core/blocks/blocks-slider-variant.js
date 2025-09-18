32a1cd7abd78e033345a99fc63bec6a3d8a8581c
Tisha Soumya <tishasoumya@gmail.com>
2025-09-18 19:33:50 +0530

    Slider block refresh content button (#655)

--- frontend/cypress/tests/core/blocks/blocks-slider-variant.js
+++ frontend/cypress/tests/core/blocks/blocks-slider-variant.js
@@ -116,4 +116,78 @@ context('Blocks Acceptance Tests', () => {
       .should('have.attr', 'src')
       .and('include', '/document/my-image/');
   });
+
+  // Refresh source content button test
+  it('As editor I can refresh the slider block content', () => {
+    cy.visit('/document/edit');
+    cy.wait('@schema');
+    cy.get('.block .slate-editor [contenteditable=true]').click();
+    cy.get('.button .block-add-button').click({ force: true });
+    cy.get('.blocks-chooser .mostUsed .button.slider')
+      .contains('Slider')
+      .click({ force: true });
+    cy.get('#field-variation').click().type('simple{enter}');
+    cy.get(
+      '.objectbrowser-field[aria-labelledby^="fieldset-default-field-label-href-0-"] button[aria-label="Open object browser"]',
+    ).click();
+
+    cy.get('[aria-label="Select Blue Orchids"]').dblclick();
+    cy.get('[id^="field-head_title-1-"]').click().clear().type('Head title');
+    cy.get('[id^="field-buttonText-5-"]').click().clear().type('Button text');
+    // Select the checkbox
+    cy.get('[id^="field-hideButton-6-"]')
+      .check({ force: true })
+      .should('be.checked');
+
+    cy.get('[id^="field-hideButton-6-"]')
+      .uncheck({ force: true })
+      .should('not.be.checked');
+
+    cy.get('.align-buttons button[aria-label="Right"]').click();
+    cy.get('.align-buttons button[aria-label="Left"]').click();
+
+    // add 2nd slider block Override Title, Description Image.
+    cy.get('.add-item-button-wrapper button').click();
+
+    cy.get(
+      '.olw-item-wrapper .olw-item-title-bar[aria-label="Collapse item #2"]',
+    )
+      .parents('.olw-item-wrapper')
+      .within(() => {
+        cy.get(
+          '.objectbrowser-field[aria-labelledby^="fieldset-default-field-label-href-0-"] button[aria-label="Open object browser"]',
+        ).click();
+      });
+
+    cy.get('[aria-label="Select Blue Orchids"]').dblclick();
+    cy.get(
+      '.olw-item-wrapper .olw-item-title-bar[aria-label="Collapse item #2"]',
+    )
+      .parents('.olw-item-wrapper')
+      .within(() => {
+        cy.get('[id^="field-title-2-"]').click().clear().type('Override Title');
+        cy.get('[id^="field-description-3-"]')
+          .click()
+          .clear()
+          .type('Override Description');
+        cy.get(
+          '.objectbrowser-field[aria-labelledby^="fieldset-default-field-label-preview_image-4-"] button[aria-label="Open object browser"]',
+        ).click();
+      });
+    cy.get('[aria-label="Select My Image"]').dblclick();
+
+    cy.wait(500);
+
+    cy.get('.refresh.slider button').click();
+
+    //testing Override Title, Description Image.
+    cy.get('[id^="field-title-2-"]')
+      .should('be.visible')
+      .should('not.have.value', 'Override Title');
+
+    cy.get('[id^="field-description-3-"]')
+      .should('be.visible')
+      .should('contain.value', 'are growing on the mountain tops');
+    cy.get('#toolbar-save').click();
+  });
 });
---
+++ frontend/packages/volto-light-theme/news/+addRefreshContentButtonSliderBlock.feature
@@ -0,0 +1,1 @@
+Add Refresh Content button for slider block and cypress test for the same @Tishasoumya-02
