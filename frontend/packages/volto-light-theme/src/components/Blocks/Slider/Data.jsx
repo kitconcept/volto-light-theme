88ac7403cc71329689d5dd3d1edc64515c58f485
Alok Kumar <ialokkumarsingh0@gmail.com>
2025-09-25 12:43:05 +0530

    Ensure the Refresh Content button correctly loads the preview image w… (#667)
    Co-authored-by: Víctor Fernández de Alba <sneridagh@gmail.com>

--- frontend/packages/volto-light-theme/src/components/Blocks/Slider/Data.jsx
+++ frontend/packages/volto-light-theme/src/components/Blocks/Slider/Data.jsx
@@ -28,6 +28,16 @@ const messages = defineMessages({
   },
 });
 
+function getImageField(resp) {
+  if (!resp) return null;
+
+  if (resp.preview_image_link) return 'preview_image_link';
+  if (resp.preview_image) return 'preview_image';
+  if (resp.image) return 'image';
+
+  return null;
+}
+
 const SliderData = (props) => {
   const {
     block,
@@ -48,16 +58,20 @@ const SliderData = (props) => {
       '@type': resp?.['@type'],
       Description: resp?.description,
       Title: resp.title,
-      hasPreviewImage: resp?.preview_image ? true : false,
+      hasPreviewImage: getImageField(resp) ? true : false,
       head_title: resp.head_title ?? null,
-      image_field: resp?.preview_image
-        ? 'preview_image'
-        : resp?.image
-          ? 'image'
-          : null,
+      image_field: getImageField(resp),
       image_scales: {
         preview_image: [resp?.preview_image],
         image: [resp?.image],
+        preview_image_link: resp?.preview_image_link
+          ? [
+              {
+                ...resp?.preview_image_link?.['image_scales']?.image?.[0],
+                base_path: resp?.preview_image_link?.['@id'],
+              },
+            ]
+          : [],
       },
       title: resp.title,
     };
