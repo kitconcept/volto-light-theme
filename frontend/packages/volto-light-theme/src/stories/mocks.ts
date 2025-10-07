0f2c749170c4155539a5b89cbe4e8fa2eaff3263
David Glick <david@glicksoftware.com>
2025-10-07 01:28:36 -0700

    Replace 'head title' with 'kicker' in all example content (#687)

--- frontend/packages/volto-light-theme/src/stories/mocks.ts
+++ frontend/packages/volto-light-theme/src/stories/mocks.ts
@@ -98,7 +98,7 @@ export const teaserBlock = {
         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea.',
       getRemoteUrl: null,
       hasPreviewImage: true,
-      head_title: 'Head title',
+      head_title: 'Kicker',
       image_field: 'preview_image',
       image_scales: {
         preview_image: [
@@ -412,7 +412,7 @@ export const gridBlock = {
       '@type': 'teaser',
       description:
         'Lorem ipsum dolor sit amet adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.',
-      head_title: 'Head title ',
+      head_title: 'Kicker',
       href: [
         {
           '@id': '.',
@@ -425,7 +425,7 @@ export const gridBlock = {
           title: 'Block: Teaser',
           getRemoteUrl: null,
           hasPreviewImage: true,
-          head_title: 'Head Title',
+          head_title: 'Kicker',
           image_field: 'preview_image',
           image_scales: {
             preview_image: [
@@ -502,7 +502,7 @@ export const gridBlock = {
       '@type': 'teaser',
       description:
         'Lorem ipsum dolor sit amet adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.',
-      head_title: 'Head title ',
+      head_title: 'Kicker',
       href: [
         {
           '@id': '.',
@@ -515,7 +515,7 @@ export const gridBlock = {
           Title: 'Block: Grid',
           getRemoteUrl: null,
           hasPreviewImage: true,
-          head_title: 'Head Title',
+          head_title: 'Kicker',
           image_field: 'preview_image',
           image_scales: {
             preview_image: [
