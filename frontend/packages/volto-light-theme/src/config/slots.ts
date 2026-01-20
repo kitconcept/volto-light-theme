c115c45f7b23f6bc45a65dcc237513676691e34a
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-01-20 11:49:04 +0100

    Transfer kitconcept.footer to kitconcept.core

--- frontend/packages/volto-light-theme/src/config/slots.ts
+++ frontend/packages/volto-light-theme/src/config/slots.ts
@@ -3,15 +3,8 @@ import Theming from '../components/Theming/Theming';
 import FooterLogos from '../components/Footer/slots/FooterLogos';
 import FollowUsLogoAndLinks from '../components/Footer/slots/FollowUsLogoAndLinks';
 import Colophon from '../components/Footer/slots/Colophon';
-import CoreFooter from '../components/Footer/slots/CoreFooter';
 import StickyMenu from '../components/StickyMenu/StickyMenu';
 import Anontools from '../components/Anontools/Anontools';
-import type { Content } from '@plone/types';
-
-export function hasInheritedBehavior(behavior: string) {
-  return ({ content }: { content: Content }) =>
-    Object.keys(content?.['@components']?.inherit || {}).includes(behavior);
-}
 
 export default function install(config: ConfigType) {
   config.registerSlotComponent({
@@ -36,13 +29,6 @@ export default function install(config: ConfigType) {
     name: 'footerLogos',
     slot: 'preFooter',
     component: FooterLogos,
-  });
-
-  config.registerSlotComponent({
-    name: 'coreFooter',
-    slot: 'footer',
-    component: CoreFooter,
-    predicates: [hasInheritedBehavior('kitconcept.footer')],
   });
 
   config.registerSlotComponent({
