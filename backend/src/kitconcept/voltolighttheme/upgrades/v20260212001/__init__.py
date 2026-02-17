89e5193cc159ddddd662a84a159848b6cf477d90
Alok Kumar <ialokkumarsingh0@gmail.com>
2026-02-17 15:58:05 +0530

    Primary colour selection should not change the search page or post footer colour. (#791)

---
+++ backend/src/kitconcept/voltolighttheme/upgrades/v20260212001/__init__.py
@@ -0,0 +1,19 @@
+from plone import api
+
+
+OLD_FIELD = "primary_foreground_color"
+NEW_FIELD = "header_foreground"
+
+
+def migrate_primary_to_header_foreground(context):
+    catalog = api.portal.get_tool("portal_catalog")
+    brains = catalog(
+        object_provides="kitconcept.voltolighttheme.behaviors.customizations.ISiteThemeCustomizationSettings"
+    )
+    for brain in brains:
+        obj = brain.getObject()
+        primary_foreground_color = getattr(obj, OLD_FIELD, None)
+        if primary_foreground_color is not None:
+            setattr(obj, NEW_FIELD, primary_foreground_color)
+            setattr(obj, OLD_FIELD, None)
+            obj.reindexObject()
